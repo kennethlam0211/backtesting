801a90d0f822da779aab4e1ed72b761fff2cd75c
kennethlam0211 <kennethlam0211@gmail.com>
2026-09-25 17:46:45 +0800

    params for RL and preprocessing.py

---
+++ params/params.py
@@ -0,0 +1,34 @@
+import datetime
+
+
+
+NORM_FACTOR = 320 #{'1':50,'15':80,'30':100,'60':120,'720':300,'W':1500}
+
+WINDOW_SIZE = 20#40
+
+COMMISSION = 0.6
+
+WINDOWS = [20] # 1minute >>60 mins
+
+FEATURES = ['fvg','std','atr','sma_rsi','sma','bbands','price_session']
+
+UNIT_TAS = ['std','atr','UD','sma_rsi','bbands','bband_targets']
+
+DATA_PATH = r'C:\Users\kennethlam\Desktop\small_projects\data_cleaning\IB_trading\IB_trading\users\LCW\RL_trading\dow\data'
+
+FREQS = ['1','15','60','720']
+
+BEGIN_DATE = datetime.datetime(2023, 1, 1, 0, 0, 0)
+
+
+
+
+
+
+
+
+
+
+
+
+
